38224007460a1aad00db826a769fca6f1a1f8cfc
Terence Dixon <terencep.dixon@gmail.com>
2022-10-06 23:43:42 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { useNavigate } from 'react-router-dom';
+import About from './Pages/About';
 
 function App() {
+  let navigate = useNavigate();
+  let component
+  switch (window.location){
+    case '/':
+      component = <App/>
+      break;
+    case '/about':
+      component = <About/>
+      break;
+    default:
+      break;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Container>
+          <h1>Obsidian Asphodel</h1>
+          <div>
+              <h3>Will you enter?</h3>
+              <Button className='btn' onClick={(e) => {
+                e.preventDefault();
+                console.log('clicked');
+                {navigate('/about')};
+              }}>Enter</Button>
+          </div>
+        </Container>
+      </>
   );
 }
 
